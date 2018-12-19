8db49919c50ff1c309edc777d9d601811d5c0483
William Song <williams@kingbright.com>
2018-12-19 15:16:36 +0800

    JAVA and Kotlin variable, data type

--- src/main/java/com/KB/Hello.java
+++ src/main/java/com/KB/Hello.java
@@ -6,6 +6,31 @@ public class Hello {
         Person p = new Person();
         p.hello();
 
+
+        int age = 19;
+//      short
+//      long
+
+        char c = '我';
+        byte b = 120;
+
+//        float weight = 66.5; 會有錯誤
+        float weight = 66.5f;
+        double hieght = 1.7;
+
+        boolean adult = true;
+        boolean enroll = true;
+
+//        字串
+        String name = "Tom";
+//      Reference data type
+
+        Integer age2 = 19;
+        Character c2 = 'A';
+        Float weight2 = 66.3f;
+        Double height2 = 1.5;
+        Boolean adult2 = true;
+
     }
 
 }
--- src/main/kotlin/com/KB/Hello.kt
+++ src/main/kotlin/com/KB/Hello.kt
@@ -5,6 +5,17 @@ fun main(args: Array<String>) {
 //    Human().hello();
     val h = Human();
     h.hello();
+
+    val age : Int= 19;
+    val age2 = 122;
+    //age =19
+
+    var weight = 77.7f;
+    weight = 75.5f
+
+    var name : String;
+    name = "Will";
+
 }
 
 class Human {
